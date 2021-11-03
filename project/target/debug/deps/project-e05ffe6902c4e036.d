955b3c65c22f4b612540a79768d31dcb20b9c08b
santaclz <rabit0110115@gmail.com>
2021-11-03 20:56:49 +0100

    Added help page

--- project/target/debug/deps/project-e05ffe6902c4e036.d
+++
@@ -1,5 +0,0 @@
-/home/nya/faks/razvoj/rpo_project/project/target/debug/deps/project-e05ffe6902c4e036: src/main.rs
-
-/home/nya/faks/razvoj/rpo_project/project/target/debug/deps/project-e05ffe6902c4e036.d: src/main.rs
-
-src/main.rs:
--- project/target/debug/project.d
+++
@@ -1,1 +0,0 @@
-/home/nya/faks/razvoj/rpo_project/project/target/debug/project: /home/nya/faks/razvoj/rpo_project/project/src/main.rs
