bad7db70a30bfa1454363e00e7b88a87dc69f105
AlexBrence <alexbrence@protonmail.com>
2021-11-23 18:10:04 +0100

    added bash command support

--- project/src/main.rs
+++ project/src/main.rs
@@ -6,21 +6,36 @@ mod ptrace;
 mod program;
 mod header_info;
 mod process_info;
-mod terminal;
 
 extern crate termion;       // for colors, style
 extern crate libc;
-extern crate capstone;
 
-use capstone::prelude::*;
-use libc::{WEXITSTATUS, WIFEXITED, WIFSIGNALED, WTERMSIG};
+use libc::{WEXITSTATUS, WIFEXITED, WIFSIGNALED, WTERMSIG, backtrace};
 use program::Program;
+use sysinfo::ProcessExt;
 use termion::{color, style};
-use termion::clear::All;
-use termion::cursor::Goto;
-use std;
-use std::{env, io, process, str::Split, ffi::CString};
+use std::{self, env, ffi::CString, io, io::Write, os::unix::prelude::OsStringExt, process,
+          process::{Command, ExitStatus, Output, Stdio}, str::{Split,from_utf8}, thread, 
+          fmt};
 
+
+fn print_prompt() {
+    /*
+     * Creates nice looking hot and sexy prompt
+     */
+
+    // print!("{}{}(", );
+    print!("{}╭─({}fdb", color::Fg(color::Green), style::Reset);
+    io::Write::flush(&mut io::stdout())
+        .expect("[ERROR] flush failed");
+
+    println!("{})", color::Fg(color::Green));
+
+    print!("╰─ > {}", style::Reset);
+    io::Write::flush(&mut io::stdout())
+        .expect("[ERROR] flush failed");
+
+}
 
 
 fn get_input() -> String {
@@ -36,7 +51,92 @@ fn get_input() -> String {
     return user_input.trim().to_string();
 }
 
-fn run_config(program_exec: &String, program_args: Vec<&str>) {
+
+
+fn check_for_bash_command(input: &String) -> Option<String> {
+    let mut is_found: Option<usize>; 
+    let mut is_found_reverse: Option<usize>; 
+
+    // Check for $(<command>) format
+    is_found = input.find("$(");
+    is_found_reverse = input.rfind(")");
+
+    if is_found != None {
+        match is_found {
+            Some(p) => {
+                match is_found_reverse {
+                    Some(rp) => {
+                        // let command = &input[p + 2..rp];
+                        Some(input[p + 2..rp].to_string())
+                    },
+                    // If closing paranthesis not found
+                    None => {
+                        println!("Invalid command.");
+                        None
+                    },
+                }
+            },
+            None => todo!()
+        }
+    }
+    else {
+        // Check for `<command>` format
+        is_found = input.find("`");
+        is_found_reverse = input.rfind("`");
+
+        match is_found {
+            Some(p) => {
+                match is_found_reverse {
+                    Some(rp) => {
+                        // let command = &input[p + 2..rp];
+                        Some(input[p + 1..rp].to_string())
+                    },
+                    None => {
+                        println!("Invalid command.");
+                        None
+                    }
+                }
+            },
+            None => None
+        }
+    }
+}
+
+fn get_bash_command_output(mut bash_command_vec: &Vec<&str>) -> Result<Vec<String>, String> { 
+    // Get only args from original vector
+    let mut args_vec: Vec<&str> = bash_command_vec[1..].to_vec();
+    let mut output: Output;
+
+    // Execute 
+    let mut executed_command = Command::new(bash_command_vec[0])
+                                        .args(args_vec.into_iter())
+                                        .output();
+
+
+    // Check if everything Ok
+    let output = match executed_command {
+        Ok(o) => o,
+        Err(e) => return Err(format!("Could not execute the bash command.\n{}\n", e)),
+    };
+
+    // Convert u8 to String, return vector to append later on in main
+    if output.status.success() {
+        let to_utf8 = String::from_utf8_lossy(&output.stdout[..output.stdout.len() - 1]);
+        let utf8_to_vec: Vec<&str> = to_utf8.split(' ').collect::<Vec<&str>>();
+        let string_vec = utf8_to_vec.iter().map(|x| x.to_string()).collect();
+        Ok(string_vec)
+    }
+    else {
+        // Print out bash error
+        let err = String::from_utf8_lossy(&output.stderr);
+        return Err(String::from(format!("Bash command was not executed.\n{}", err)));
+    }
+
+}
+
+
+
+fn run_config(program_exec: &String, program_args: Vec<String>) {
     let program_pid: libc::pid_t;
     let mut arg_values: Vec<i8> = Vec::new();
     let mut args_ptr: Vec<*const i8> = Vec::new();
@@ -44,7 +144,7 @@ fn run_config(program_exec: &String, program_args: Vec<&str>) {
 
     // Turn args into i8 array separated by nullbytes
     for i in 0..program_args.len() {
-        let cs = CString::new(program_args[i]).unwrap();
+        let cs = CString::new(program_args[i].as_str()).unwrap();
         let cv: Vec<u8> = cs.into_bytes_with_nul();
         let mut tmp: Vec<i8> = cv.into_iter().map(|c| c as i8).collect::<_>();
         arg_values.append(&mut tmp);
@@ -63,6 +163,7 @@ fn run_config(program_exec: &String, program_args: Vec<&str>) {
     }
     // Terminate reading more arguments (argv stops here)
     args_ptr.push(std::ptr::null());
+
 
     unsafe {
         program_pid = libc::fork();
@@ -88,17 +189,18 @@ fn run_config(program_exec: &String, program_args: Vec<&str>) {
         // Check the exit code
         if WIFEXITED(status) {
             let x: i32 = WEXITSTATUS(status);
-            println!("Program exited with code: {}", x);
+            println!("Program exited with code: {}\n", x);
         }
         // Or if abnormal exit e.g. segfault
         if WIFSIGNALED(status) {
-            println!("Program ended with kill: {}", WTERMSIG(status));
+            println!("Program ended with signal: {}\n", WTERMSIG(status));
         }
     }
 }
 
-fn main() {
 
+
+fn main() {
     // TODO make this prettier
     // Read args and set filename
     let args: Vec<String> = env::args().collect();
@@ -106,6 +208,7 @@ fn main() {
         println!("Please specify a filename");
         process::exit(1);
     }
+
     // Reading file into buffer
     let filename = args[1].clone();
     let buffer = static_info::load_file(filename.clone());
@@ -114,36 +217,58 @@ fn main() {
     // Reference to the file_object is further passed to functions
     let file_object = static_info::parse_file(&buffer);
 
-    // Create Capstone object
-    let capstone_obj = Capstone::new()
-        .x86()
-        .mode(arch::x86::ArchMode::Mode64)
-        .syntax(arch::x86::ArchSyntax::Intel)
-        .detail(true)
-        .build()
-        .expect("Failed to create Capstone object");
-
     let mut running: bool = true;
 
     println!("Welcome to Feri Debugger. For commands and functions type 'help'.\n");
 
     // Main loop
     while running {
-        terminal::print_prompt();
-        //terminal::key_commands();
+        print_prompt();
         let input = get_input();
-        let mut spliterator: Split<char> = input.as_str().split(' '); // Iterator through arguments
 
+        // Filter out bash commands if they exist
+        let mut bash_command: String = String::new();
+        let mut bash_command_vec: Vec<&str> = Vec::new();
+
+        let contains_bash_command = match check_for_bash_command(&input) {
+            Some(c) => {
+                bash_command = c;
+                bash_command_vec = bash_command.split(' ').collect();
+                true
+            },
+            None => {
+                false
+            }
+        };
+
+        let mut spliterator: Split<char> = input.as_str().split(' '); 
         match spliterator.next() {
             Some(arg) => match arg {
                 "help" | "h" => print_help(),
                 "run" | "r" => {
-                        let mut vec_program_args: Vec<&str> = Vec::new();
+                        let mut vec_program_args: Vec<String> = Vec::new();
+
                         // Add filename to argv[0]
                         let fname = &filename.to_string();
-                        vec_program_args.push(fname);
-                        while let Some(program_args) = spliterator.next() {
-                            vec_program_args.push(program_args);
+                        vec_program_args.push(fname.clone());
+
+                        // Check if bash command is used for args input
+                        if contains_bash_command {
+                            let mut tmp_vec = get_bash_command_output(&mut bash_command_vec);
+                            match tmp_vec {
+                                Ok(mut o) => {
+                                    vec_program_args.append(&mut o);
+                                },
+                                Err(e) => {
+                                    println!("{}[ERROR]{} {}", color::Fg(color::Red), style::Reset, e);
+                                    continue;
+                                }
+                            }
+                        }
+                        else {
+                            while let Some(program_args) = spliterator.next() {
+                                vec_program_args.push(program_args.to_string());
+                            }
                         }
                         run_config(&filename, vec_program_args);
                 },
@@ -182,9 +307,8 @@ fn main() {
                 "continue" | "c" => println!("continue"),
                 "step" | "s" => println!("step"),
                 "disas" | "d" => {
-                    if let Some(func) = spliterator.next() {
-                        static_info::disassemble(func, &file_object, &buffer, &capstone_obj);
-                        //println!("dissasemble {} ", func.to_string());
+                    if let Some(func) = spliterator.next(){
+                        println!("dissasemble {} ", func.to_string());
                     }
                     else{
                         println!("not enough arguments type 'help' for help");
@@ -245,6 +369,7 @@ fn main() {
                     }
                 },
                 "stack" => println!("dump memory from current stack"),
+                "bt" => println!("List frames"), // use libc backtrace
                 "info" => {
                     if let Some(topic) = spliterator.next() {
                         match topic {
@@ -255,14 +380,10 @@ fn main() {
                         }
                     }
                     else {
-                        println!("not enouhg argumets type 'help' ");
+                        println!("not enough argumets type 'help' ");
                     }
                 },
                 "quit" | "q" => running = false,
-                "clear" =>{
-                    println!("{}",termion::clear::All);
-                    print!("{}",termion::cursor::Goto(1,1));
-                    },
                 _ => println!("This command does not exist. Type 'help' for commands and functions."),
             },
             None => todo!(),
@@ -302,7 +423,6 @@ debugger commands:
     stack                   dump memory from current stack frame
 
     info <header, process>  print information
-    clear                   clears terminal
 ";
 
     println!("{}\n", help_str);
